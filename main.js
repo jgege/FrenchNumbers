0e74ec3825afa8c780bef094dd5e242d27863c74
jgege <juhaszgegereg@gmail.com>
2016-01-20 20:24:45 +0000

    GA test

--- main.js
+++ main.js
@@ -284,15 +284,15 @@ app = (function(module, $, question_list, ga) {
         initTextToSpeech();
         question = getNewQuestion(question_list);
         showQuestion();
+        
+        $('.page-header').click(function(e) {
+            console.log('header click before event');
+            ga('send', 'event', 'Page', 'headerClick', 'General');
+            console.log('header click after event');
+        });
     }
 
     return module;
 }(app, jQuery, question_list, ga));
 
 app.init('answer_form');
-
-$('.page-header').click(function(e) {
-    console.log('header click before event');
-    ga('send', 'event', 'Page', 'headerClick', 'General');
-    console.log('header click after event');
-});
